6b146c68d4618a6d068352302d0e59bedf1eeed6
md_fazal <fazalmohammad980@gmail.com>
2023-06-25 12:34:15 +0530

    initial commit

---
+++ src/components/NewExpense/AddToExpense.js
@@ -0,0 +1,25 @@
+import NewExpense from "./NewExpense";
+import "./AddToExpense.css";
+import { useState } from "react";
+
+const AddToExpense = (props) => {
+
+    const outputForm = () =>{
+        console.log("here")
+        setDisplay(<NewExpense onAddExpense={props.onAddExpense}></NewExpense>)
+    }
+
+    const addNew = <button onClick={outputForm} className="add_to_expense__controls">Add New Expense</button>;
+    const [display, setDisplay] = useState(addNew);
+
+    
+
+	return (
+		<div className="add_to_expense">
+			{display}
+		</div>
+	);
+	// 
+};
+
+export default AddToExpense;
